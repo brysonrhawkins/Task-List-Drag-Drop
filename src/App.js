1dd518761d47b0e1d21a0815837eba9e16e4efea
Bryson Hawkins <Hawkins@Brysons-MacBook-Pro.local>
2023-03-31 22:21:08 -0600

    app complete, css styling to card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const [cards, setCards] = useState([]);
+  const [inputText, setInputText] = useState('');
+
+  const handleInput =(e)=>{
+    setInputText(e.target.value);
+  }
+
+  const handleCreateCard = ()=>{
+    if (inputText !== ''){
+      const newCard = {id: Date.now(), text: inputText};
+      setCards([...cards, newCard]);
+      setInputText('');
+    }
+  };
+
+  const handleDragStart = (e, index)=>{
+    e.dataTransfer.setData('cardIndex', index);
+  };
+
+  const handleDragOver = (e)=>{
+    e.preventDefault();
+  }
+
+  const handleDrop = (e, index) =>{
+    const cardIndex = e.dataTransfer.getData('cardIndex');
+    const newCards = [...cards];
+    const [removedCard] = newCards.splice(cardIndex, 1);
+    newCards.splice(index, 0, removedCard);
+    setCards(newCards);
+  };
+
+  const handleEraseCard =(cardIndex)=>{
+    const newCards = [...cards];
+    newCards.splice(cardIndex, 1);
+    setCards(newCards);
+  }
+
+  return(
+    <div className='App'>
+      <div className='input-container'>
+        <input
+        type="text"
+        value={inputText}
+        onChange={handleInput}
+        placeholder="Enter text for new card..."
+        />
+        <button onClick={handleCreateCard}>Create Card</button>
+      </div>
+      <div className="card-container">
+        {cards.map((card, index)=>(
+          <div key={card.id} className="card" draggable onDragStart={(e)=>handleDragStart(e, index)}
+          onDragOver={handleDragOver} onDrop={(e)=>handleDrop(e, index)}>{card.text}
+          <button onClick={handleEraseCard}>Erase Task</button>
+
+          </div>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
